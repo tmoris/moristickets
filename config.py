1d93be11f14d57d5a180d9bc675b7734f17a3d5a
Tibenkana Moris <moristotau@gmail.com>
2023-07-12 08:55:12 +0400

    Cofigured basic flask app settings

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+
+class Config(object):
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+    # Basic Secret_key configurations
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    DEBUG = os.environ.get("DEBUG") or True
+    # Database URl configurations
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATaBASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
