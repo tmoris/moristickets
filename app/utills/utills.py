53ee494fa3f781434126a1249d705ffb42c8bbd4
Tibenkana Moris <moristotau@gmail.com>
2023-07-18 08:42:27 +0400

    Created utills

---
+++ app/utills/utills.py
@@ -0,0 +1,25 @@
+import os
+import secrets
+from PIL import Image
+from flask import current_app, flash
+
+
+def image_saver(image, folder):
+    try:
+        img_name_base = secrets.token_hex(8)
+        _, file_ext = os.path.splitext(image.filename)
+        image_name = img_name_base + file_ext
+        image_path = os.path.join(
+            current_app.root_path, "static/img/" + folder, image_name
+        )
+
+        # Image resizing
+        img_size = (400, 400)
+        i = Image.open(image)
+        i.thumbnail(img_size)
+        i.save(image_path, quality=95)
+        return image_name
+    except Exception as e:
+        flash(f"An error occurred while saving the image: {e}")
+        print(f"Exception while saving image: {e}")
+        return None
