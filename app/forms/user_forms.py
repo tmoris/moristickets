c3e6378437f294157100f6b1deec96f7b2fd58ae
Tibenkana Moris <moristotau@gmail.com>
2023-08-02 15:05:26 +0400

    Created TestimonialForm, ContactForm

--- app/forms/user_forms.py
+++ app/forms/user_forms.py
@@ -68,4 +68,36 @@ class UpdateAccountForm(FlaskForm):
         validators=[FileAllowed(["jpg", "jpeg", "gif", "webp", "png", "tif"])],
     )
     bio = TextAreaField("About You (Biography)")
+    address = StringField("Address", validators=[Optional()])
+    phone_number = StringField("Phone Number", validators=[Optional()])
+    company_name = StringField("Company Name", validators=[Optional()])
+    company_logo = FileField(
+        "Upload Company Logo",
+        validators=[FileAllowed(["jpg", "jpeg", "png", "gif", "webp", "tif"])],
+    )
     submit = SubmitField("Update Profile")
+
+
+class TestimonialForm(FlaskForm):
+    """
+    This class represents a form for submitting a testimonial.
+    It requires the testimonial content and the author's role.
+    """
+
+    # Form to submit a testimonial
+    testimony = TextAreaField("Testimony", validators=[DataRequired()])
+    role = StringField("Author's Role", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class ContactForm(FlaskForm):
+    """
+    This class represents a form for sending a contact message.
+    It requires fields for the user's name, email, and the message they wish to send.
+    """
+
+    # Form to allow a user to send a contact message
+    name = StringField("Name", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    message = TextAreaField("Message", validators=[DataRequired()])
+    submit = SubmitField("Send")
