2ea12d944387dee2c5cf38b728677216d7352dfd
Tibenkana Moris <moristotau@gmail.com>
2023-08-05 08:56:52 +0400

    Update config.py db_uri

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 import psycopg2
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,12 +11,6 @@ class Config(object):
     # Basic Secret_key configurations
     SECRET_KEY = os.environ.get("SECRET_KEY")
     DEBUG = os.environ.get("DEBUG") or True
-
     # Database URl configurations
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
-            "postgres://", "postgresql://", 1
-        )
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
